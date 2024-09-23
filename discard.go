d5cfddf49ba26cae210ecee42b7eade3df9525bd
Felipe Mariotti <felipe.mtt95@gmail.com>
2024-09-23 20:18:38 -0300

    refactor(noop): Turn noop into a type alias to use it as a constant

--- discard.go
+++ discard.go
@@ -1,20 +1,21 @@
 package stdlog
 
-type noop struct{}
+type noop string
 
-func Discard() Logger {
-	return &noop{}
-}
+const Discard noop = "noop"
 
-func (n noop) Named(name string) Logger {
-	return n
-}
+func (n noop) Named(name string) Logger { return n }
+
 func (n noop) SetLevel(level Level) {}
-func (n noop) Debug(msg string, keysAndValues ...any) {
 
-}
-func (n noop) Info(msg string, keysAndValues ...any)                  {}
-func (n noop) Warning(msg string, keysAndValues ...any)               {}
-func (n noop) Error(err error, msg string, keysAndValues ...any)      {}
-func (n noop) Fatal(msg string, keysAndValues ...any)                 {}
+func (n noop) Debug(msg string, keysAndValues ...any) {}
+
+func (n noop) Info(msg string, keysAndValues ...any) {}
+
+func (n noop) Warning(msg string, keysAndValues ...any) {}
+
+func (n noop) Error(err error, msg string, keysAndValues ...any) {}
+
+func (n noop) Fatal(msg string, keysAndValues ...any) {}
+
 func (n noop) FatalError(err error, msg string, keysAndValues ...any) {}
