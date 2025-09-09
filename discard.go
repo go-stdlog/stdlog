0088781cb29bf78f64a8bfa8e3a6a26c91b7ab12
Felipe Mariotti <felipe.mtt95@gmail.com>
2025-09-08 22:02:22 -0300

    refactor(noop): Ensure even key-value pairs on noop usage

--- discard.go
+++ discard.go
@@ -14,16 +14,33 @@ func (n noop) Leveled(level Level) Logger { return n }
 
 func (n noop) Skipping(count uint) Logger { return n }
 
-func (n noop) Debug(msg string, keysAndValues ...any) {}
+func (n noop) Debug(msg string, keysAndValues ...any) {
+	assertKvs(LevelDebug.String(), keysAndValues...)
+}
 
-func (n noop) Info(msg string, keysAndValues ...any) {}
+func (n noop) Info(msg string, keysAndValues ...any) {
+	assertKvs(LevelInfo.String(), keysAndValues...)
+}
 
-func (n noop) Warning(msg string, keysAndValues ...any) {}
+func (n noop) Warning(msg string, keysAndValues ...any) {
+	assertKvs(LevelWarning.String(), keysAndValues...)
+}
 
-func (n noop) Error(err error, msg string, keysAndValues ...any) {}
+func (n noop) Error(err error, msg string, keysAndValues ...any) {
+	assertKvs(LevelError.String(), keysAndValues...)
+}
 
-func (n noop) Fatal(msg string, keysAndValues ...any) { os.Exit(1) }
+func (n noop) Fatal(msg string, keysAndValues ...any) {
+	assertKvs(LevelFatal.String(), keysAndValues...)
+	os.Exit(1)
+}
 
-func (n noop) FatalError(err error, msg string, keysAndValues ...any) { os.Exit(1) }
+func (n noop) FatalError(err error, msg string, keysAndValues ...any) {
+	assertKvs(LevelFatal.String()+"_ERROR", keysAndValues...)
+	os.Exit(1)
+}
 
-func (n noop) WithFields(keysAndValues ...any) Logger { return n }
+func (n noop) WithFields(keysAndValues ...any) Logger {
+	assertKvs("WithFields", keysAndValues...)
+	return n
+}
