8590a3d8f0ced204ce70e02dd6532c5aa03ec100
Vito Sartori <hey@vito.io>
2026-06-02 16:31:55 -0300

    feat: Introduce SetFatalBehavior

--- base.go
+++ base.go
@@ -27,6 +27,20 @@ func (l Level) String() string {
 	}
 }
 
+// FatalBehavior determines how Logger behaves when Logger.Fatal or
+// Logger.FatalError is called. See Logger.SetFatalBehavior
+type FatalBehavior uint8
+
+const (
+	// FatalExits makes Logger invoke os.Exit with status 1 after printing a
+	// message with a Fatal log level. This is the default mode.
+	FatalExits FatalBehavior = iota
+
+	// FatalPanics makes Logger emit a panic with the same message as provided
+	// to the logger method. This does not invoke os.Exit.
+	FatalPanics
+)
+
 type Logger interface {
 	// Named returns a new logger with its previous name followed by a dot,
 	// followed by the provided name.
@@ -45,6 +59,13 @@ type Logger interface {
 	// Skipping returns a copy of the current logger that will skip a given
 	// number of call stacks.
 	Skipping(count uint) Logger
+
+	// SetFatalBehavior defines how Logger behaves when Fatal or FatalError is
+	// called. By default, Logger uses FatalExits, which means that any call to
+	// a fatal logger level calls os.Exit with status 1. The other option,
+	// FatalPanics, panics after printing the log message to the configured
+	// output, allowing deferred functions to be executed.
+	SetFatalBehavior(behavior FatalBehavior)
 
 	Debug(msg string, keysAndValues ...any)
 	Info(msg string, keysAndValues ...any)
